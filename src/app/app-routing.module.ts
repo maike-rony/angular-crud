d6cdbaeaa687f71e5500ab6213c1babc2a0a331f
Maike Rony Schuerzoski <maike_rony@hotmail.com>
2020-10-28 02:25:12 -0300

    Second Commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,6 +1,8 @@
+import { ProductUpdateComponent } from './components/product/product-update/product-update.component';
 import { ProductCreateComponent } from './components/product/product-create/product-create.component';
-import { NgModule } from '@angular/core';
+import { ProductDeleteComponent } from './components/product/product-delete/product-delete.component';
 import { Routes, RouterModule } from '@angular/router';
+import { NgModule } from "@angular/core";
 
 import { HomeComponent } from './views/home/home.component';
 import { ProductCrudComponent } from './views/product-crud/product-crud.component';
@@ -17,6 +19,14 @@ const routes: Routes = [
 {
   path: "products/create",
   component: ProductCreateComponent
+},
+{
+  path: "products/update/:id",
+  component: ProductUpdateComponent
+},
+{
+  path: "products/delete/:id",
+  component: ProductDeleteComponent
 }
 ];
 
--- src/app/components/product/product-create/product-create.component.ts
+++ src/app/components/product/product-create/product-create.component.ts
@@ -1,4 +1,8 @@
+import { Product } from './../product.model';
+import { ProductService } from './../product.service';
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+
 
 @Component({
   selector: 'app-product-create',
@@ -7,9 +11,28 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProductCreateComponent implements OnInit {
 
-  constructor() { }
+  product: Product = {
+    name: '',
+    price: null
+  }
+
+  constructor(
+              private productService: ProductService, 
+              private router: Router)  { }
 
   ngOnInit(): void {
+    
+  }
+
+  createProduct(): void {
+      this.productService.create(this.product).subscribe(() => {
+      this.productService.showMessage("Produto Cadastrado com Sucesso!")
+      this.router.navigate(['/products'])
+    })
+  }
+
+  cancel(): void {
+    this.router.navigate(['/products'])
   }
 
 }
